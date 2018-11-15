695b2ec2bab26b0586e3b42e3e7c0b583e036179
Gitanjali Tiwari <Gitanjali.Tiwari@NIIT-Tech.com>
2018-11-15 12:23:22 +0530

    Locathost to Trd 509

--- BookStoreWebService/Models/BookDB/BookStoreDBContext.cs
+++ BookStoreWebService/Models/BookDB/BookStoreDBContext.cs
@@ -30,7 +30,7 @@ namespace BookStoreWebService.Models.BookDB
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("server=localhost;database=BookStoreDB;trusted_connection=yes");
+                optionsBuilder.UseSqlServer("server=trd-509;database=BookStoreDB;trusted_connection=yes");
             }
         }
 
